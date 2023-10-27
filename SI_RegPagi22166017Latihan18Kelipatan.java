07350d3c2d2ca5f9dfb18b38ab568c09ce605287
silkyafina <148930513+silkyafina@users.noreply.github.com>
2023-10-27 21:23:47 +0700

    Add files via upload

---
+++ SI_RegPagi22166017Latihan18Kelipatan.java
@@ -0,0 +1,28 @@
+package si_regpagi.pkg22166017.latihan18.kelipatan;
+/**
+ *
+ * @author User
+ * Nama : Silky Afina Saly
+ * NIM  : 22166017
+ * Prodi: Sistem Informasi
+ * Deskripsi Program: Program untuk menampilkan kelipatan 3.5 dengan batas 35
+ */
+public class SI_RegPagi22166017Latihan18Kelipatan {
+
+    public static void main(String[] args) {
+        System.out.println("========== Program Kelipatan 3.5 ==========");
+        // deklarasi variabel
+        double i;
+        // iterasi kepilatan 3.5 dengan batas 35
+      for(i=3.5; i<=35.0;i+=3.5){
+          if (i%3.5==0)
+        // output
+        System.out.println(i);
+      }    
+System.out.println("(Developed by: Silky Afina Saly)");
+     
+    }
+}
+     
+    
+
